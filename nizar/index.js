b5bb2a8bdb02b8a934ca9be1915cb96709e0ec52
Soudqi Nizar <soudqinizar@gmail.com>
2022-06-16 09:54:09 +0200

    Inizio Express

--- nizar/index.js
+++ nizar/index.js
@@ -1,10 +1,14 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write("Prova iniziale HTTP"); 
-  res.end(); 
-}).listen(8080); 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
