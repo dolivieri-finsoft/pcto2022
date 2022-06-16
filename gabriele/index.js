d18d7dd60f97c6a90c57ac1303f1173eb6b24e2f
Gabriele Cancemi <87067748+gabrielecancemi@users.noreply.github.com>
2022-06-16 09:58:40 +0200

    add gitignore

--- gabriele/index.js
+++ gabriele/index.js
@@ -1,7 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Server HTTP");
-  res.end();
-}).listen(8080);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
