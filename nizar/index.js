02c374c044a8089a07fcef8cc888a5ef22d6f37c
Soudqi Nizar <soudqinizar@gmail.com>
2022-06-16 09:44:29 +0200

    Server HTTP
    Creazione del Server HTTP

--- nizar/index.js
+++ nizar/index.js
@@ -1,1 +1,10 @@
-console.log('Prova Soudqi');
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write("Prova iniziale HTTP"); 
+  res.end(); 
+}).listen(8080); 
+
+
+
