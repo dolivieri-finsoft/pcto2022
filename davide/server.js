6260d9c4991d03795d14d6722f3094b4e5073fb6
Davide Trapani <davidetrapani2004@gmail.com>
2022-06-17 09:25:45 +0200

    add

--- davide/server.js
+++ davide/server.js
@@ -3,6 +3,7 @@ var express = require('express'),
 const path = require('path');
 
 app.use(express.static("public"));
+
 app.get('/',function (req,res) {
         res.sendfile(path.join(__dirname, '/public/index.html'));
         })
