1ffa91153045f2efe1cd905bbdf069ab946239d7
Davide Trapani <davidetrapani2004@gmail.com>
2022-06-17 10:28:22 +0200

    commit

--- davide/server.js
+++ davide/server.js
@@ -5,8 +5,36 @@ const path = require('path');
 app.use(express.static("public"));
 
 app.get('/',function (req,res) {
-        res.sendfile(path.join(__dirname, '/public/index.html'));
+        res.sendFile(path.join(__dirname, '/public/login'));
         })
 .listen(8080);
+
+app.get('/login',function (req,res) {
+    res.sendFile(path.join(__dirname, './public/login'));
+});
+
+app.get('/home',function (req,res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/form', function (req, res){
+    res.sendFile(path.join(__dirname, './public/pages/form/'))
+})
+
+app.get('/prieces', function (req, res){
+    res.sendFile(path.join(__dirname, './public/pages/prieces/'))
+})
+
+app.get('/room', function (req, res){
+    res.sendFile(path.join(__dirname, './public/pages/room/'))
+})
+
+app.get('/service', function (req, res){
+    res.sendFile(path.join(__dirname, './public/pages/service/'))
+})
+
+app.get('/todo', function (req, res){
+    res.send('ciao');
+})
 
 console.log("Server hostato su http://localhost:8080");
