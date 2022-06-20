b2168062c343abefbd0dc130a5d421c5f7ad16e7
Gabriele Cancemi <87067748+gabrielecancemi@users.noreply.github.com>
2022-06-20 16:44:39 +0200

    Fine TODO

--- gabriele/TODO/index.js
+++ gabriele/TODO/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const fs = require("fs");
+
 
 
 router.get('/',function(req,res){
@@ -9,14 +11,73 @@ router.get('/',function(req,res){
 });
 
 router.get('/request',function(req,res){
-    const fs = require("fs");
-    fs.readFile('./dati/todo.json', (err, jsonString) => {
+    fs.readFile('./dati/todo.json', "utf-8" ,(err, jsonString) => {
         let o = JSON.parse(jsonString);
         res.send(o);
         
 
     });
 });
+
+router.get('/write',function(req,res){
+
+    var myObject = Read();
+    
+    var newData = {
+      "cosa": `${req.query.cosa}`,
+      "stato": `${req.query.stato}`
+    };
+    
+    myObject.push(newData);
+    Write(myObject);
+
+});
+
+router.get('/delete',function(req,res){
+
+  var myObject = Read();
+
+  
+  for(var i = 0; i < myObject.length; i++){
+    if(myObject[i].cosa == req.query.cosa){
+      myObject.splice(i, 1);
+      break;
+    }
+  }
+  
+  Write(myObject);
+
+});
+
+router.get('/change',function(req,res){
+
+  var myObject = Read();
+
+  
+  for(var i = 0; i < myObject.length; i++){
+    if(myObject[i].cosa == req.query.cosa){
+      myObject[i].stato = "done";
+      break;
+    }
+  }
+  
+  Write(myObject);
+
+});
+
+
+function Read(){
+  var data = fs.readFileSync("./dati/todo.json", "utf-8");
+  return myObject = JSON.parse(data);
+}
+
+function Write(myObject){
+  fs.writeFile("./dati/todo.json", JSON.stringify(myObject), (err) => {
+    if (err) throw err;
+    console.log("ok");
+  });
+}
+
 
 
 app.use('/', router);
