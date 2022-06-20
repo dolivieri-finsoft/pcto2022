6040f89ca0e8a2fdf620f9104b012587e458aaab
Matteo Piazzi <matteo.piazzi@salesianiverona.it>
2022-06-20 17:42:48 +0200

    minor change

--- matteo_p/todo/todo.js
+++ matteo_p/todo/todo.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 2424
+const express = require('express');
+const app = express();
+const port = 2424;
+const bp = require('body-parser');
+app.use(bp.urlencoded({extended:true}));
+app.use(bp.json());
+
+const fs = require('fs');
 
 app.use(express.static('./'));
-
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/todo.html");
 })
@@ -12,5 +16,9 @@ app.listen(port, () => {
   console.log("Listening on port "+ port)
 })
 
-
-
+app.post('/rewrite', function(req, res){
+  var aux = JSON.stringify(req.body);
+  fs.writeFile('todoList.json', aux, err =>{
+      if(err) throw err;
+  });
+})
