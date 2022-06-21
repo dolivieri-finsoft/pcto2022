8db9016e0392cd86109614f20f44697b6ef866e3
andree13 <cullino.andrea13@gmail.com>
2022-06-21 10:52:18 +0200

    halo

--- andrea_c/LISTASPESA/js/script.js
+++ andrea_c/LISTASPESA/js/script.js
@@ -2,23 +2,50 @@ const todoList = () => {
     fetch("/json/data.json")
         .then(response => response.json())
         .then(data => {
-            html = '<tr><th id="tt"></th></tr>';
+            if (window.location.href == "http://localhost:3000/fatte") {
+                html = '<tr><th>QUEST COMPLETATE</th><th>DELETE QUEST</th></tr>';
+            }
+            else if (window.location.href == "http://localhost:3000/dafare") {
+                html = '<tr><th>QUEST IN CORSO</th><th>CHANGE STATUS</th><th>DELETE QUEST</th></tr>';
+            }
             for (var i = 0; i < data.length; i++) {
                 const element = data[i];
-                // url=window.location.href;
-                if(element.stato=="done" && window.location.href=="http://localhost:3000/fatte")
-                {
-                    document.getElementById("tt")=element.stato;
+                if (element.stato == "done" && window.location.href == "http://localhost:3000/fatte") {
                     html += '<tr><td>' + element.cosa + '</td><td></td></tr>';
                 }
-                else if(element.stato=="todo" && window.location.href =="http://localhost:3000/dafare")
-                {
-                    document.getElementById("tt")=element.stato;
-                    html += '<tr><td>' + element.cosa + '</td><td></td></tr>';                }
-              
+                else if (element.stato == "todo" && window.location.href == "http://localhost:3000/dafare") {
+                    html += '<tr><td>' + element.cosa + '</td><td></td><td></td></tr>';
+                }
+
             }
             document.getElementById("todoList").innerHTML = html;
         })
         .catch(error => console.log(error));
 }
 document.onloadeddata = todoList();
+
+const saveTodo = () => {
+    var cosa = document.getElementById('chore').value;
+    var stato = 'todo';
+
+    console.log(`Cosa: ${cosa}, Stato: ${stato}`);
+
+    if (cosa=="")
+    {
+        alert("ERRORE: compilare correttamente");
+    } 
+    else
+    {
+        fetch("/json/data.json" + "cmd=newTodo&cosa=" + cosa + "&stato=" + stato)
+            .then(response => {
+                if (response.status == 200 && response.statusText == "OK") {
+                    console.log('added');
+                }
+                else
+                {
+                    console.log('error');
+                }
+            })
+            window.location.reload();
+    }
+}
