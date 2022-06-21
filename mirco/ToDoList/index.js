0dd5db6dc3d58107f6ac3d2bcd88f5846ad9aed8
Mirco Bonazzi <mirco.bonazzi@salesianiverona.it>
2022-06-21 16:04:18 +0200

    todoList con change

---
+++ mirco/ToDoList/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('source'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/source/content.html');
+});
+
+app.post('/add',(req,res) => {
+    let toAdd = req.body.value;
+    const newTask = {cosa: toAdd, stato:"todo"};
+    let json = fs.readFileSync('./source/todo.json');
+    const data = JSON.parse(json);
+    data.todoList.push(newTask);
+    json = JSON.stringify(data, null, 2);
+    fs.writeFileSync('./source/todo.json',json);
+})
+
+app.post('/remove',(req,res) => {
+    const search = req.body.value;
+    let json = fs.readFileSync('./source/todo.json');
+    const data = JSON.parse(json);
+    let list = data.todoList;
+
+    for(let x of list){
+        if(search == x.cosa){
+            list.splice(list.indexOf(x),1);
+            break;
+        }
+    }
+
+    json = JSON.stringify(data, null, 2);
+    fs.writeFileSync('./source/todo.json',json);
+})
+
+app.post('/move',(req,res) => {
+    const search = req.body.value;
+    let json = fs.readFileSync('./source/todo.json');
+    const data = JSON.parse(json);
+    let list = data.todoList;
+
+    for(let x of list){
+        if(search == x.cosa){
+            if(x.stato == "todo"){
+                x.stato = "done";
+                break;
+            }
+            x.stato = "todo";
+            break;
+        }
+    }
+    json = JSON.stringify(data);
+    fs.writeFileSync('./source/todo.json',json);
+})
+
+app.post('/rename', (req,res) => {
+    const data = JSON.parse(fs.readFileSync('./source/todo.json'));
+
+    for(let x of data.todoList){
+        if(req.body.value == x.cosa){
+            x.cosa = req.body.newValue;
+            break;
+        }
+    }
+    const json = JSON.stringify(data);
+    fs.writeFileSync('./source/todo.json',json);
+})
+
+app.get('/*', (req,res) => {
+    res.redirect('/');
+});
+
+app.listen(PORT,() => {console.log("Server su porta " + PORT)});
