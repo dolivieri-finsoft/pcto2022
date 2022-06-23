5cb2c8525c2797ab89f7ab37611194ede933aaf7
fmattia <mafontana@it-present.com>
2022-06-23 15:10:30 +0200

    cambio da json a mysql

--- mattia/static/js/deleteTodo.js
+++ mattia/static/js/deleteTodo.js
@@ -1,8 +1,8 @@
 const todoList = () => {
-    fetch("/json?" + "cmd=getList")
+    fetch("/mysql?" + "cmd=getList")
         .then(response => response.json())
         .then(data => {
-            if (data.length == 0) {
+            if (data.status == 200 && data.response == 0) {
                 html = '<select id="selectCosa" class="formSelect" name="cosa" disabled>';
                 html += '<option>No data</option>';
                 html += '</select>';
@@ -10,8 +10,8 @@ const todoList = () => {
                 document.getElementById("btnElimina").disabled = true;
             } else {
                 html = '<select id="selectCosa" class="formSelect" name="cosa">';
-                for (var i = 0; i < data.length; i++) {
-                    const element = data[i];
+                for (var i = 0; i < data.response.length; i++) {
+                    const element = data.response[i];
                     html += '<option value="' + element.cosa + '">' + element.cosa + '</option>';
                 }
                 html += '</select>';
@@ -28,10 +28,14 @@ const aggiornaLista = () => {
 const deleteTodo = () => {
     var select = document.getElementById("selectCosa");
     var cosa = select.value;
-    fetch("/json?" + "cmd=deleteTodo&cosa=" + cosa)
-        .then(response => {
-            if (response.status == 200 && response.statusText == "OK") {
-                window.location.href = '/todo';
+
+    fetch("/mysql?" + "cmd=deleteTodo&cosa=" + cosa)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status == 200) {
+                window.location.href = '/home';
+            }else if (data.status == 500){
+                alert(data.error);
             }
         })
         .catch(error => console.log(error));
