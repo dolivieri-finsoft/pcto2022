166093fa77d848aea3f5a3165da10653dd605815
Soudqi Nizar <soudqinizar@gmail.com>
2022-06-28 13:29:11 +0200

    Modifiche pagina admin

--- nizar/TODO/index.js
+++ nizar/TODO/index.js
@@ -31,6 +31,10 @@ router.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/static/index.html'));
 });
 
+router.get('/admin',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/admin/index.html'));
+});
+
 router.get('/request', function(req,res){
 
   conn.query('SELECT * FROM pcto2022.lista', function(err , result)
@@ -48,7 +52,7 @@ router.get('/write', function(req,res){
   var sql1 = "SELECT cosa FROM pcto2022.lista where cosa = '" + req.query.cosa + "';";
   conn.query(sql1, function (err, result) {
     if (result.length == 0){
-      var sql = "INSERT INTO pcto2022.lista (cosa, stato, username) VALUES ('" + req.query.cosa + "', '" + req.query.stato + "', '" + localStorage.username + "');";
+      var sql = "INSERT INTO pcto2022.lista (cosa, stato, username) VALUES ('" + req.query.cosa + "', '" + req.query.stato + "', '" + req.query.username + "');";
       conn.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Aggiunto: ✔");
@@ -127,7 +131,7 @@ router.get('/signin', function(req,res){
 });
 
 router.get('/login', function(req,res){ 
-  var sql1 = "SELECT password FROM pcto2022.utenti where username = '" + req.query.username + "';";
+  var sql1 = "select password, ruolo from pcto2022.utenti where username = '" + req.query.username + "';";
   conn.query(sql1, function (err, result) {
     if(err) throw err;
     res.send(result);
@@ -144,6 +148,16 @@ router.get('/deleteAccount', function(req,res){
     res.send(result);
   });
 
+});
+
+
+
+router.get('/lista',function(req,res){
+  var sql = "SELECT * FROM pcto2022.utenti";
+  conn.query(sql, function (err, result){
+    if(err) throw err;
+    res.send(result);
+  });
 });
 
 
