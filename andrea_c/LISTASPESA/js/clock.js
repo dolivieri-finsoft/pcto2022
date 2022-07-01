338f255465c03dcc38791f48fad43b7411f689e9
andree13 <cullino.andrea13@gmail.com>
2022-07-01 15:02:44 +0200

    final

--- andrea_c/LISTASPESA/js/clock.js
+++ andrea_c/LISTASPESA/js/clock.js
@@ -4,7 +4,25 @@ g = data.getDate() + "/";
 m = data.getMonth() + 1 + "/";
 a = data.getFullYear();
 document.write("Oggi è il " + g + m + a + ", e sarebbe anche l'ora che tu facessi qualcosa");
-// h = data.getHours() + ":";
-// m = data.getMinutes() + ":";
-// s = data.getSeconds();
-// document.write(g + m + a + "            " + h + m + s)
+
+const controllazione = () => {
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
+    if (control == "no") {
+        alert('Loggati bastardo');
+        window.location.href = '/';
+    }
+    else if (role == 'admin'||role == 'creator') {
+        admin = document.getElementById("adminSpace");
+        admin.style.display = "block";
+    }
+}
+document.onloadeddata = controllazione();
+
+const out = () => {
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
+    localStorage.controllo = "no";
+}
--- andrea_c/LISTASPESA/js/script.js
+++ andrea_c/LISTASPESA/js/script.js
@@ -1,40 +1,58 @@
 const todoList = () => {
-    fetch("/json?" + "cmd=getList")
-        .then(response => response.json())
-        .then(data => {
-            if (window.location.href == "http://localhost:3000/fatte") {
-                html = '<tr><th>QUEST COMPLETATE</th><th>DELETE QUEST</th></tr>';
-            }
-            else if (window.location.href == "http://localhost:3000/dafare") {
-                html = '<tr><th>QUEST IN CORSO</th><th>CHANGE STATUS</th><th>DELETE QUEST</th></tr>';
-            }
-            for (var i = 0; i < data.length; i++) {
-                const element = data[i];
-                var id= element.cosa;
-                if (element.stato == "done" && window.location.href == "http://localhost:3000/fatte") {
-                    html += '<tr><td>' + element.cosa + '</td><td><button type="button" class="btn btn-outline-danger" id="' + id + '" onclick="deleteTodo(`' + element.cosa + '`)";">Delete</button></td></tr>';
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
+    if (control == "no") {
+        alert('Loggati bastardo');
+        window.location.href = '/';
+    }
+    else {
+        if (role == 'admin'||role == 'creator') {
+            admin = document.getElementById("adminSpace");
+            admin.style.display = "block";
+        }
+        fetch("/json?" + "cmd=getList&tabella=" + idaccesso)
+            .then(response => response.json())
+            .then(data => {
+                if (window.location.href == "http://localhost:3000/fatte") {
+                    html = '<tr><th>QUEST COMPLETATE</th><th>DELETE QUEST</th></tr>';
                 }
-                else if (element.stato == "todo" && window.location.href == "http://localhost:3000/dafare") {
-                    html += '<tr><td>' + element.cosa + '</td><td><button type="button" class="btn btn-outline-success" onclick="changeStatus(`' + element.cosa + '`)">Done</button></td><td><button type="button" class="btn btn-outline-danger" id="' + id + '" onclick="deleteTodo(`' + element.cosa + '`)";">Delete</button></td></tr>';
+                else if (window.location.href == "http://localhost:3000/dafare") {
+                    html = '<tr><th>QUEST IN CORSO</th><th>CHANGE STATUS</th><th>DELETE QUEST</th></tr>';
                 }
+                for (var i = 0; i < data.length; i++) {
+                    const element = data[i];
+                    var id = element.cosa;
+                    if (element.stato == "done" && window.location.href == "http://localhost:3000/fatte") {
+                        html += '<tr><td>' + element.cosa + '</td><td><button type="button" class="btn btn-outline-danger" id="' + id + '" onclick="deleteTodo(`' + element.id + '`)";">Delete</button></td></tr>';
+                    }
+                    else if (element.stato == "todo" && window.location.href == "http://localhost:3000/dafare") {
+                        html += '<tr><td>' + element.cosa + '</td><td><button type="button" class="btn btn-outline-success" onclick="changeStatus(`' + element.id + '`)">Done</button></td><td><button type="button" class="btn btn-outline-danger" id="' + id + '" onclick="deleteTodo(`' + element.id + '`)";">Delete</button></td></tr>';
+                    }
 
-            }
-            document.getElementById("todoList").innerHTML = html;
-        })
-        .catch(error => console.log(error));
+                }
+                document.getElementById("todoList").innerHTML = html;
+            })
+            .catch(error => console.log(error));
+    }
 }
 
 
 
 
 
-function aggiornaLista () {
+function aggiornaLista() {
     todoList();
     console.log('aggiornata');
+    let idaccesso = localStorage.getItem('IDaccesso');
+    console.log(idaccesso);
 }
 
-const changeStatus = (cosa) => {
-    fetch("/json?" + "cmd=changeStatus&cosa=" + cosa)
+const changeStatus = (id) => {
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
+    fetch("/json?" + "cmd=changeStatus&id=" + id + "&tabella=" + idaccesso)
         .then(response => {
             if (response.status == 200 && response.statusText == "OK") {
                 aggiornaLista();
@@ -49,38 +67,40 @@ const changeStatus = (cosa) => {
 
 
 const add = () => {
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
     var cosa = document.getElementById('chore').value;
     var stato = 'todo';
     console.log(`Cosa: ${cosa}, Stato: ${stato}`);
 
-    if (cosa=="")
-    {
+    if (cosa == "") {
         alert("ERRORE: compilare correttamente");
-    } 
-    else
-    {
-        fetch("/json?" + "cmd=newchore&cosa=" + cosa + "&stato=" + stato)
+    }
+    else {
+        fetch("/json?" + "cmd=newchore&cosa=" + cosa + "&stato=" + stato + "&tabella=" + idaccesso)
             .then(response => {
                 if (response.status == 200 && response.statusText == "OK") {
                     console.log('added');
                     // window.location.href='/dafare';
                 }
-                else
-                {
+                else {
                     console.log('error');
                 }
-                   aggiornaLista();
+                aggiornaLista();
             })
     }
 }
 
-const deleteTodo = (cosa) => {
-    // var cosa = document.getElementById(cosa);
-    fetch("/json?" + "cmd=deleteTodo&cosa=" + cosa)
+const deleteTodo = (id) => {
+    let idaccesso = localStorage.getItem('IDaccesso');
+    let control = localStorage.getItem('controllo');
+    let role = localStorage.getItem('ruolo');
+    fetch("/json?" + "cmd=deleteTodo&id=" + id + "&tabella=" + idaccesso)
         .then(response => {
             if (response.status == 200 && response.statusText == "OK") {
                 aggiornaLista();
-                
+
             }
         })
         .catch(error => console.log(error));
@@ -88,3 +108,4 @@ const deleteTodo = (cosa) => {
 
 
 document.onloadeddata = todoList();
+
